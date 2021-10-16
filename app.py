6a0780f1a4b7ee0d68993cd4ceecff5d3e6597ea
ze2 <62995130+ze-2@users.noreply.github.com>
2021-10-16 09:09:35 +0800

    restart

--- app.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from flask import Flask, render_template, redirect, url_for, request, flash, session
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
