48b4cf3caf293a3de8d264930c16314ea6f7051a
ze2 <62995130+ze-2@users.noreply.github.com>
2021-10-16 17:59:28 +0800

    autocomplete
    please :(

--- modules/firebase.js
+++ modules/firebase.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
 import { getDatabase, ref, set, onValue, get } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-database.js";
-import { deleteAllCookies, setCookie } from "./cookies.js";
 import { splitStr } from "./topics.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  setPersistence,
-  browserSessionPersistence,
-  signOut } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD2QJoM4F3HWT5gVmzd_VkHr9TGQmh9JQI",
@@ -24,126 +16,21 @@ const app = initializeApp(firebaseConfig);
 
 const db = getDatabase(app)
 
-function signUp(){
-    console.log('signing up')
-    var email = document.querySelector(".su-email").value;
-    var password = document.querySelector(".su-password").value;
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        // ...
-        console.log(user)
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-        console.log(errorCode)
-        console.log(errorMessage)
-      });
-}
-
-function signIn(){
-  const auth = getAuth();
-  keepAuth()
-  var email = document.querySelector(".si-email").value;
-  var password = document.querySelector(".si-password").value;
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      console.log("signed in")
-      console.log(user)
-      console.log(auth)
-      // ...
-      document.cookie = "user_email=" + email + "; path=./index.html";
-      document.cookie = "password=" + password + "; path=./index.html";
-      setCookie("user_email", email, "10")
-      setCookie("paddword", password, "10")
-      window.location.href='/pages/home.html'
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode)
-      console.log(errorMessage)
-    });
-
-}
-
-function autoSignIn(auth, email, password){
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      console.log("auto signed in")
-      console.log(user)
-      console.log(auth)
-      // ...
-      console.log("logged in as " + email)
-      var logged = document.querySelector('.logged-in')
-      logged.innerHTML = "logging you in!"
-      setTimeout(() => {window.location.href='/pages/home.html'}, 1000);
-
-    })
-    .catch((error) => {
-      console.log('cannot autosignin')
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode)
-      console.log(errorMessage)
-    });
-}
-
-function keepAuth(){
-  const auth = getAuth();
-  setPersistence(auth, browserSessionPersistence)
-    .then(() => {
-      // Existing and future Auth states are now persisted in the current
-      // session only. Closing the window would clear any existing state even
-      // if a user forgets to sign out.
-      // ...
-      // New sign-in will be persisted with session persistence.
-      return signInWithEmailAndPassword(auth, email, password);
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-}
-
-function logout(){
-  const auth = getAuth();
-  signOut(auth).then(() => {
-      console.log('logging out')
-      deleteAllCookies()
-      setTimeout(() => {window.location.href='../index.html'}, 1000);
-
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
-}
-
-var selectedTopic = "";
 
 function updateTopics(){
   const topRef = ref(db, "topics/");
-  onValue(topRef,(topics)=>{
+  onValue(topRef, (topics) => {
     var itemsContainer = document.querySelector(".items-container")
 
-    topics.forEach(topic=>{
+    topics.forEach(topic => {
       var div = document.createElement("div");
-        var prodPrice = document.createElement("h1");
-        var prodName = document.createElement("h2");
-        var prodDesc = document.createElement("p");
-        var storeName = document.createElement("h4");
-        var storeLocation = document.createElement("h3");
-        var country = document.createElement("h5");
-        var city = document.createElement("h5");
+      var prodPrice = document.createElement("h1");
+      var prodName = document.createElement("h2");
+      var prodDesc = document.createElement("p");
+      var storeName = document.createElement("h4");
+      var storeLocation = document.createElement("h3");
+      var country = document.createElement("h5");
+      var city = document.createElement("h5");
 
       prodPrice.innerHTML = topic.val().prodPrice;
       prodName.innerHTML = topic.val().prodName;
@@ -162,13 +49,13 @@ function updateTopics(){
       div.appendChild(city);
 
       itemsContainer.appendChild(div)
-    })
+    });
 
-  })
+  });
 }
 
-function submitTopic(prodName, prodDesc, prodPrice, storeName, storeLocation, country, city, tags){
-  set(ref(db, "topics/" + prodName),{
+function submitTopic(prodName, prodDesc, prodPrice, storeName, storeLocation, country, city, tags) {
+  set(ref(db, "topics/" + prodName), {
     prodName: prodName,
     prodDesc: prodDesc,
     prodPrice: prodPrice,
@@ -177,62 +64,59 @@ function submitTopic(prodName, prodDesc, prodPrice, storeName, storeLocation, co
     country: country,
     city: city,
     tags: splitStr(tags)
-  })
+  });
 }
 
 function createDiv(prodName, prodDesc, prodPrice, storeName, storeLocation, country, city){
-  var itemsContainer = document.querySelector("items-container")
+  var itemsContainer = document.querySelector(".items-container")
   var div = document.createElement("div");
-      var prodPrice = document.createElement("h1");
-      var prodName = document.createElement("h2");
-      var prodDesc = document.createElement("p");
-      var storeName = document.createElement("h4");
-      var storeLocation = document.createElement("h3");
-      var country = document.createElement("h5");
-      var city = document.createElement("h5");
+      var prodPriceEle = document.createElement("h1");
+      var prodNameEle = document.createElement("h2");
+      var prodDescEle = document.createElement("p");
+      var storeNameEle = document.createElement("h4");
+      var storeLocationEle = document.createElement("h3");
+      var countryEle = document.createElement("h5");
+      var cityEle = document.createElement("h5");
 
-      prodPrice.innerHTML = prodPrice;
-      prodName.innerHTML = prodName;
-      prodDesc.innerHTML = prodDesc;
-      storeName.innerHTML = storeName;
-      storeLocation.innerHTML = storeLocation;
-      country.innerHTML = country;
-      city.innerHTML = city;
+      prodPriceEle.innerHTML = prodPrice;
+      prodNameEle.innerHTML = prodName;
+      prodDescEle.innerHTML = prodDesc;
+      storeNameEle.innerHTML = storeName;
+      storeLocationEle.innerHTML = storeLocation;
+      countryEle.innerHTML = country;
+      cityEle.innerHTML = city;
 
 
-      div.appendChild(prodPrice);
-      div.appendChild(prodName);
-      div.appendChild(prodDesc);
-      div.appendChild(storeName);
-      div.appendChild(storeLocation);
-      div.appendChild(country);
-      div.appendChild(city);
+      div.appendChild(prodPriceEle);
+      div.appendChild(prodNameEle);
+      div.appendChild(prodDescEle);
+      div.appendChild(storeNameEle);
+      div.appendChild(storeLocationEle);
+      div.appendChild(countryEle);
+      div.appendChild(cityEle);
 
       // to add css classes
       // h2.classList.add('class-name')
-
-      div.addEventListener('click',(e)=>{
-        localStorage.setItem("selectedTopic", prodName);
-        console.log(selectedTopic + " is selected")
-        setTimeout(() => {window.location.href='/pages/topic.html'}, 500);
-      })
-      itemsContainer.appendChild(div)
+    itemsContainer.appendChild(div);
 }
 
-function search(tag){
-  tag = tag.toLowerCase()
-  console.log('searching for topics with tag ' + tag)
+function search(search){
+  search = search.toLowerCase()
   get(ref(db), `topics/`).then((topics) => {
     if (topics.exists()) {
-      topics.forEach(topic=>{
-        topic.forEach(topMeta=>{
+      topics.forEach(topic => {
+        topic.forEach(topMeta => {
           console.log(topMeta.val().tags)
-          if(topMeta.val().tags.includes(tag)){
-            console.log("adding div")
+          if (topMeta.val().tags.includes(search)){
+            console.log("trying to add div")
             createDiv(topMeta.val().prodName, topMeta.val().prodDesc, topMeta.val().prodPrice, topMeta.val().storeName, topMeta.val().storeLocation, topMeta.val().country, topMeta.val().city);
-          }
-        })
-      })
+          };
+          if (topMeta.val().prodName.toLowerCase() == search){
+            console.log("trying to add div")
+            createDiv(topMeta.val().prodName, topMeta.val().prodDesc, topMeta.val().prodPrice, topMeta.val().storeName, topMeta.val().storeLocation, topMeta.val().country, topMeta.val().city);
+          };
+        });
+      });
     } else {
       console.log("No data available");
     }
@@ -241,4 +125,4 @@ function search(tag){
   });
 }
 
-export { signUp, signIn, autoSignIn, logout, submitTopic, updateTopics, search}
+export {submitTopic, updateTopics, search}
--- modules/home.js
+++ modules/home.js
@@ -1,10 +1,3 @@
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js";
-import { logout, updateTopics } from "./firebase.js";
-const logoutButton = document.querySelector(".logout");
-logoutButton.addEventListener('click',logout)
-console.log(getAuth())
-
-
+import { updateTopics } from "./firebase.js";
 
 updateTopics();
